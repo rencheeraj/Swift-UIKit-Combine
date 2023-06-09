cc68b1b3406b78ad3cb725101854ce3e19e2ff23
rencheeraj <rencheerajm@gmail.com>
2023-06-09 14:24:34 +0530

    PassThrough Subject in Button Action

--- Swift UIKit Combine/Swift UIKit Combine/ViewController.swift
+++ Swift UIKit Combine/Swift UIKit Combine/ViewController.swift
@@ -6,26 +6,41 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
 
+    private var model = [ResponceModel]()
+    let passThroughSubject = PassthroughSubject<String, Error>()
     @IBOutlet weak var submitBtn: UIButton!
     @IBOutlet weak var displayLabel: UILabel!
+    var observer = Set<AnyCancellable>()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.passThroughSubject.sink { value in
+            print(value)
+        } receiveValue: { value in
+            DispatchQueue.main.async {
+                print(value)
+                self.displayLabel.text = value
+            }
+        }.store(in: &observer)
+
+        
+    }
+
+    @IBAction func submitBtnAction(_ sender: Any) {
         
         APICaller.shared.getJsonResult() {result in
             switch result{
             case .success(let json):
                 print(json)
+                self.passThroughSubject.send("\(json.punchline)")
             case .failure(let error):
                 print(error)
             }
         }
-    }
-
-    @IBAction func submitBtnAction(_ sender: Any) {
     }
     
 }
